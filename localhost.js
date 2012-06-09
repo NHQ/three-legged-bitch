229ba7f70d3c60144625e805b28c7e0abb6c8e81
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2012-06-08 18:42:41 -0700

    added readme

--- localhost.js
+++ localhost.js
@@ -2,7 +2,7 @@ var request = require('request')
 	,	http = require('http')
 	,	url = require('url')
 	,	qs = require('querystring')
-	,	remoteHost = '74.207.246.247' // change this to your server's domain or IP address
+	,	remoteHost = 'you remote host' // change this to your server's domain or IP address
 ;
 
 http.createServer(function(req, res){
